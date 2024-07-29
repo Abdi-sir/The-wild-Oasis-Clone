bbdbc38eaa82ef5696af7862dcd03b2dc18c4f9e
Abdi <122712513+Abdi-sir@users.noreply.github.com>
2024-07-29 15:58:15 +0300

    Add files via upload

---
+++ services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabaseUrl = '';
+const supabaseKey =
+  '';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
